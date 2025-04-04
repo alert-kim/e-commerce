815e8e3ffaeb31fe8021cf3f13fd014c14d73009
alert-kim <kyeong236@gmail.com>
2025-04-04 09:07:18 +0900

    feat: 인기상품 조회 API 스펙

--- src/main/kotlin/kr/hhplus/be/server/controller/product/ProductController.kt
+++ src/main/kotlin/kr/hhplus/be/server/controller/product/ProductController.kt
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.RestController
 import java.time.Instant
 
 @RestController
-@RequestMapping("/products")
 @Tag(name = "Product API", description = "상품 관련 API")
 class ProductController {
 
@@ -40,7 +39,7 @@ class ProductController {
             ),
         ]
     )
-    @GetMapping
+    @GetMapping("/products")
     fun getProducts(): ProductsResponse = ProductsResponse(
         products = listOf(
             ProductResponse(
@@ -63,4 +62,50 @@ class ProductController {
             )
         )
     )
+
+
+    @Operation(
+        summary = "인기 상품 조회",
+        description = "3일간 판매량이 높은 5개의 상품 조회, 제일 많이 팔린 순으로 반환"
+    )
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "인기 상품 목록 조회 성공",
+                content = [
+                    Content(
+                        mediaType = "application/json",
+                        schema = Schema(
+                            implementation = ProductsResponse::class,
+                        )
+                    )
+                ]
+            ),
+        ]
+    )
+    @GetMapping("/products:popular")
+    fun getPopularProducts(): ProductsResponse = ProductsResponse(
+        products = listOf(
+            ProductResponse(
+                id = 1L,
+                status = ProductStatus.ON_SALE,
+                name = "상품1",
+                description = "상품1 설명",
+                price = 10000,
+                stock = 10,
+                createdAt = Instant.now(),
+            ),
+            ProductResponse(
+                id = 2L,
+                status = ProductStatus.ON_SALE,
+                name = "상품2",
+                description = "상품2 설명",
+                price = 20000,
+                stock = 20,
+                createdAt = Instant.now(),
+            )
+        )
+    )
+
 }
