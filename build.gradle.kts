4fc73a0ef091d1a767f605536ab2a988710539ce
alert-kim <kyeong236@gmail.com>
2025-04-08 06:06:45 +0900

    [week3/basic] build: mockK 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
 	testImplementation("org.testcontainers:junit-jupiter")
 	testImplementation("org.testcontainers:mysql")
+	testImplementation("io.mockk:mockk:1.13.7")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
 	// Swagger
