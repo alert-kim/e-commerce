a1c8c61b78cc67ec29051da32611e2fb2efb7175
alert-kim <kyeong236@gmail.com>
2025-05-04 11:25:12 +0900

    [week6/basic] test: 주문 Facade 테스트 정리

--- src/test/kotlin/kr/hhplus/be/server/application/order/OrderFacadeTest.kt
+++ src/test/kotlin/kr/hhplus/be/server/application/order/OrderFacadeTest.kt
@@ -17,6 +17,8 @@ import kr.hhplus.be.server.testutil.mock.CouponMock
 import kr.hhplus.be.server.testutil.mock.OrderCommandMock
 import kr.hhplus.be.server.testutil.mock.OrderMock
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
@@ -41,122 +43,130 @@ class OrderFacadeTest {
     @MockK(relaxed = true)
     private lateinit var orderPaymentProcessor: OrderPaymentProcessor
 
-    @Test
-    fun `order - 쿠폰이 있는 경우`() {
-        val couponId = CouponMock.id()
-        val command = OrderCommandMock.facade(couponId = couponId).also { spyk(it) }
-        val userId = UserId(command.userId)
-        val orderId = OrderMock.id()
-        val order = OrderMock.view(id = orderId, userId = userId, couponId = couponId)
-        every { orderCreationProcessor.createOrder(any()) } returns OrderCreationProcessorResult(orderId)
-        every { orderService.get(orderId.value) } returns order
+    @Nested
+    @DisplayName("주문")
+    inner class Order {
+        @Test
+        @DisplayName("쿠폰이 있는 주문 처리")
+        fun withCoupon() {
+            val couponId = CouponMock.id()
+            val command = OrderCommandMock.facade(couponId = couponId).also { spyk(it) }
+            val userId = UserId(command.userId)
+            val orderId = OrderMock.id()
+            val order = OrderMock.view(id = orderId, userId = userId, couponId = couponId)
+            every { orderCreationProcessor.createOrder(any()) } returns OrderCreationProcessorResult(orderId)
+            every { orderService.get(orderId.value) } returns order
 
-        val result = orderFacade.order(command)
+            val result = orderFacade.order(command)
 
-        assertThat(result.order).isEqualTo(order)
-        verifyOrder {
-            command.validate()
-            orderCreationProcessor.createOrder(
-                CreateOrderProcessorCommand(command.userId)
-            )
-            command.productsToOrder.forEach {
-                orderProductProcessor.placeOrderProduct(
-                    PlaceOrderProductProcessorCommand(
+            assertThat(result.order).isEqualTo(order)
+            verifyOrder {
+                command.validate()
+                orderCreationProcessor.createOrder(
+                    CreateOrderProcessorCommand(command.userId)
+                )
+                command.productsToOrder.forEach {
+                    orderProductProcessor.placeOrderProduct(
+                        PlaceOrderProductProcessorCommand(
+                            orderId = orderId,
+                            productId = it.productId,
+                            unitPrice = it.unitPrice,
+                            quantity = it.quantity,
+                        )
+                    )
+                }
+                orderService.get(orderId.value)
+                orderCouponProcessor.applyCouponToOrder(
+                    ApplyCouponProcessorCommand(
                         orderId = orderId,
-                        productId = it.productId,
-                        unitPrice = it.unitPrice,
-                        quantity = it.quantity,
+                        userId = userId,
+                        couponId = couponId.value
                     )
                 )
-            }
-            orderService.get(orderId.value)
-            orderCouponProcessor.applyCouponToOrder(
-                ApplyCouponProcessorCommand(
-                    orderId = orderId,
-                    userId = userId,
-                    couponId = couponId.value
-                )
-            )
-            orderService.get(orderId.value)
-            orderPaymentProcessor.processPayment(
-                PayOrderProcessorCommand(
-                    orderId = orderId,
-                    userId = order.userId,
-                    totalAmount = order.totalAmount
-                )
-            )
-            orderService.get(orderId.value)
-        }
-    }
-
-    @Test
-    fun `order - 쿠폰이 없는 경우`() {
-        val command = OrderCommandMock.facade(couponId = null).also { spyk(it) }
-        val userId = UserId(command.userId)
-        val orderId = OrderMock.id()
-        val order = OrderMock.view(id = orderId, userId = userId, couponId = null)
-        every { orderCreationProcessor.createOrder(any()) } returns  OrderCreationProcessorResult(orderId)
-        every { orderService.get(orderId.value) } returns order
-
-        val result = orderFacade.order(command)
-
-        assertThat(result.order).isEqualTo(order)
-        verifyOrder {
-            command.validate()
-            orderCreationProcessor.createOrder(
-                CreateOrderProcessorCommand(command.userId)
-            )
-            command.productsToOrder.forEach {
-                orderProductProcessor.placeOrderProduct(
-                    PlaceOrderProductProcessorCommand(
+                orderService.get(orderId.value)
+                orderPaymentProcessor.processPayment(
+                    PayOrderProcessorCommand(
                         orderId = orderId,
-                        productId = it.productId,
-                        unitPrice = it.unitPrice,
-                        quantity = it.quantity,
+                        userId = order.userId,
+                        totalAmount = order.totalAmount
                     )
                 )
+                orderService.get(orderId.value)
             }
-            orderService.get(orderId.value)
-            orderPaymentProcessor.processPayment(
-                PayOrderProcessorCommand(
-                    orderId = orderId,
-                    userId = order.userId,
-                    totalAmount = order.totalAmount
+        }
+
+        @Test
+        @DisplayName("쿠폰이 없는 주문 처리")
+        fun withoutCoupon() {
+            val command = OrderCommandMock.facade(couponId = null).also { spyk(it) }
+            val userId = UserId(command.userId)
+            val orderId = OrderMock.id()
+            val order = OrderMock.view(id = orderId, userId = userId, couponId = null)
+            val creationResult = OrderCreationProcessorResult(orderId)
+            every { orderCreationProcessor.createOrder(any()) } returns creationResult
+            every { orderService.get(orderId.value) } returns order
+
+            val result = orderFacade.order(command)
+
+            assertThat(result.order).isEqualTo(order)
+            verifyOrder {
+                command.validate()
+                orderCreationProcessor.createOrder(
+                    CreateOrderProcessorCommand(command.userId)
                 )
+                command.productsToOrder.forEach {
+                    orderProductProcessor.placeOrderProduct(
+                        PlaceOrderProductProcessorCommand(
+                            orderId = orderId,
+                            productId = it.productId,
+                            unitPrice = it.unitPrice,
+                            quantity = it.quantity,
+                        )
+                    )
+                }
+                orderService.get(orderId.value)
+                orderPaymentProcessor.processPayment(
+                    PayOrderProcessorCommand(
+                        orderId = orderId,
+                        userId = order.userId,
+                        totalAmount = order.totalAmount
+                    )
+                )
+                orderService.get(orderId.value)
+            }
+            verify(exactly = 0) {
+                orderCouponProcessor.applyCouponToOrder(any())
+            }
+        }
+
+        @Test
+        @DisplayName("주문 상품 확보는 productId 기준으로 정렬되어 처리")
+        fun processOrderProductsInOrder() {
+            val command = mockk<OrderFacadeCommand>(relaxed = true)
+            every { command.productsToOrder } returns listOf(
+                OrderCommandMock.productToOrder(productId = 3L, unitPrice = 10_000.toBigDecimal(), quantity = 2),
+                OrderCommandMock.productToOrder(productId = 1L, unitPrice = 15_000.toBigDecimal(), quantity = 1),
+                OrderCommandMock.productToOrder(productId = 2L, unitPrice = 10_000.toBigDecimal(), quantity = 3)
             )
-            orderService.get(orderId.value)
-        }
-        verify(exactly = 0) {
-            orderCouponProcessor.applyCouponToOrder(any())
-        }
-    }
+            val orderId = OrderMock.id()
+            val order = OrderMock.view(id = orderId)
 
-    @Test
-    fun `order - placeStocks - productId 기준으로 정렬되어 처리된다`() {
-        val command = mockk<OrderFacadeCommand>(relaxed = true)
-        every { command.productsToOrder } returns listOf(
-            OrderCommandMock.productToOrder(productId = 3L, unitPrice = 10_000.toBigDecimal(), quantity = 2),
-            OrderCommandMock.productToOrder(productId = 1L, unitPrice = 15_000.toBigDecimal(), quantity = 1),
-            OrderCommandMock.productToOrder(productId = 2L, unitPrice = 10_000.toBigDecimal(), quantity = 3)
-        )
-        val orderId = OrderMock.id()
-        val order = OrderMock.view(id = orderId)
+            every { orderService.createOrder(any<CreateOrderCommand>()) } returns orderId
+            every { orderService.get(orderId.value) } returns order
 
-        every { orderService.createOrder(any<CreateOrderCommand>()) } returns orderId
-        every { orderService.get(orderId.value) } returns order
+            orderFacade.order(command)
 
-        orderFacade.order(command)
-
-        verifyOrder {
-            orderProductProcessor.placeOrderProduct(withArg<PlaceOrderProductProcessorCommand> {
-                assertThat(it.productId).isEqualTo(1L)
-            })
-            orderProductProcessor.placeOrderProduct(withArg<PlaceOrderProductProcessorCommand> {
-                assertThat(it.productId).isEqualTo(2L)
-            })
-            orderProductProcessor.placeOrderProduct(withArg<PlaceOrderProductProcessorCommand> {
-                assertThat(it.productId).isEqualTo(3L)
-            })
+            verifyOrder {
+                orderProductProcessor.placeOrderProduct(withArg<PlaceOrderProductProcessorCommand> {
+                    assertThat(it.productId).isEqualTo(1L)
+                })
+                orderProductProcessor.placeOrderProduct(withArg<PlaceOrderProductProcessorCommand> {
+                    assertThat(it.productId).isEqualTo(2L)
+                })
+                orderProductProcessor.placeOrderProduct(withArg<PlaceOrderProductProcessorCommand> {
+                    assertThat(it.productId).isEqualTo(3L)
+                })
+            }
         }
     }
 }
