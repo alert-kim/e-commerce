93d9ab02b45b8a8b38eed69ac256aaff9975cef9
alert-kim <kyeong236@gmail.com>
2025-05-09 00:17:54 +0900

    [week6/advanced] feat: 유저 캐싱

--- src/main/kotlin/kr/hhplus/be/server/domain/user/UserService.kt
+++ src/main/kotlin/kr/hhplus/be/server/domain/user/UserService.kt
@@ -1,15 +1,18 @@
 package kr.hhplus.be.server.domain.user
 
+import kr.hhplus.be.server.common.cache.CacheNames
 import kr.hhplus.be.server.domain.user.exception.NotFoundUserException
 import kr.hhplus.be.server.domain.user.repository.UserRepository
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 
 @Service
 class UserService(
-    private val repository: UserRepository,
+    private val userCacheReader: UserCacheReader,
 ) {
+    @Cacheable(value = [CacheNames.USER], key = "#id")
     fun get(id: Long): UserView {
-        val user = repository.findById(id)
+        val user = userCacheReader.getOrNull(id)
             ?: throw NotFoundUserException("by id: $id")
         return UserView.from(user)
     }
