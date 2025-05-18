6e525197745b2178fc44841bda48d6b1caf5206b
alert-kim <kyeong236@gmail.com>
2025-05-18 22:45:48 +0900

    [wee7/basic/feedback] feat: Async 어노테이션 사용시, 지정한 Executor사용

--- src/main/kotlin/kr/hhplus/be/server/config/AsyncConfig.kt
+++ src/main/kotlin/kr/hhplus/be/server/config/AsyncConfig.kt
@@ -2,7 +2,21 @@ package kr.hhplus.be.server.config
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.context.annotation.Bean
+import java.util.concurrent.Executor
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 
 @Configuration
 @EnableAsync
-class AsyncConfig
+class AsyncConfig {
+    @Bean
+    fun taskExecutor(): Executor {
+        val executor = ThreadPoolTaskExecutor()
+        executor.corePoolSize = 10
+        executor.maxPoolSize = 20
+        executor.queueCapacity = 100
+        executor.setThreadNamePrefix("EventAsync-")
+        executor.initialize()
+        return executor
+    }
+}
